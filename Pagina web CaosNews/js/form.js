de4054f71eb5aa234f3c6505c0a5ee828c2ebc72
furanshis <fra.santanderv@alumnos.duoc.cl>
2021-05-16 15:45:12 -0400

    formulario

---
+++ Pagina web CaosNews/js/form.js
@@ -0,0 +1,7 @@
+function largo(){
+    if (document.getElementById("#direccion").lenght < 0){
+        document.getElementById("#p-direccion").innerHTML = "El campo no debe estar vacio"
+        alert("ingrese datos")
+
+    }
+}
