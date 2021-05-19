87b645620cc56217cfc75c95e76ec25ba6ce40b1
furanshis <fra.santanderv@alumnos.duoc.cl>
2021-05-18 22:51:51 -0400

    Validación de formulario
    valide si los campos obligatorios están vacíos.

--- Pagina web CaosNews/js/form.js
+++ Pagina web CaosNews/js/form.js
@@ -1,7 +1,56 @@
-function largo(){
-    if (document.getElementById("#direccion").lenght < 0){
-        document.getElementById("#p-direccion").innerHTML = "El campo no debe estar vacio"
-        alert("ingrese datos")
+$(document).ready(
+    function(){
+        $("#form-motivo").submit(function(){
+            var comuna = $("#comuna").val()
+            if(comuna.length == 0){
+                $('#p-comuna').html("*Este campo no debe estar vacío")
+                $("#comuna").css("border-color", "red");
+                event.preventDefault()
+            }else{
+                $('#p-comuna').html("*")
+            }
 
+            if($('#direccion').val().length == 0){
+                $('#p-direccion').html("*Este campo no debe estar vacío")
+                $("#direccion").css("border-color", "red");
+                event.preventDefault()
+            }else{
+                $('#p-direccion').html('')
+            }
+
+            if($('#nombre').val().length == 0){
+                $('#p-nombre').html("*Este campo no debe estar vacío")
+                $("#nombre").css("border-color", "red");
+                event.preventDefault()
+            }else{
+                $('#p-nombre').html('')
+            }
+
+            if($('#app').val().length == 0){
+                $('#p-app').html("*Este campo no debe estar vacío")
+                $("#app").css("border-color", "red");
+                event.preventDefault()
+            }else{
+                $('#p-app').html('')
+            }
+
+            if($('#apm').val().length == 0){
+                $('#p-apm').html("*Este campo no debe estar vacío")
+                $("#apm").css("border-color", "red");
+                event.preventDefault()
+            }else{
+                $('#p-apm').html('')
+            }
+
+            if($('#email').val().length == 0){
+                $('#p-email').html("*Este campo no debe estar vacío")
+                $("#email").css("border-color", "red");
+                event.preventDefault()
+            }else{
+                $('#p-email').html('')
+            }
+            
+
+        });
     }
-}
+)
