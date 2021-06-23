2df02c62b70bc812c2e91f2220744c56cb8162ff
furanshis <fra.santanderv@alumnos.duoc.cl>
2021-06-23 19:21:06 -0400

    models.py
    agregue la conexión a la base de datos y creé dos entidades

--- caosNews_Django/caosNews_Django/settings.py
+++ caosNews_Django/caosNews_Django/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'caosNews_Django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '127.0.0.1/xe',
+        'USER': 'Django_user',
+        'PASSWORD': 'django',
+        'TEST': {
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp'
+        }
     }
 }
 
--- caosNews_Django/core/models.py
+++ caosNews_Django/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    idUsuario = models.IntegerField(primary_key=True, verbose_name="Id de usuario")
+    nombreUsuario = models.CharField(max_length=50, verbose_name="Nombre de usuario")
+    apellidoUsuario = models.CharField(max_length=50, verbose_name="Nombre de usuario")
+    contraseñaUsuario = models.CharField(max_length=20, verbose_name="Contraseña de usuario")
+
+    def __str__(self):
+        return self.nombreUsuario
+
+class Noticia(models.Model):
+    idNoticia = models.IntegerField(primary_key=True, verbose_name="Id de la noticia")
+    tituloNoticia = models.CharField(max_length=50, verbose_name="titulo de la noticia")
+    epigrafe = models.CharField(max_length=100, verbose_name="epígrafe de la noticia")
+    cuerpo = models.CharField(max_length=500, verbose_name="Cuerpo de la noticia")
+    fotoNoticia = models.BinaryField(null=True)
